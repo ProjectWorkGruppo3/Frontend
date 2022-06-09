e35bbeb64be58c7a7219d029a10d353f2d07862f
Saliou <gayesaliou29@gmail.com>
2022-06-09 10:26:01 +0200

    chore: create login UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
+import '../styles/font.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ pages/login.tsx
@@ -0,0 +1,131 @@
+import type { NextPage } from 'next';
+import {
+  Card,
+  Container,
+  Center,
+  TextInput,
+  PasswordInput,
+  Button,
+  Group,
+  Title,
+  Box,
+  Alert,
+} from '@mantine/core';
+import { MdOutlineAlternateEmail, MdPassword } from 'react-icons/md';
+
+import { useForm } from '@mantine/form';
+import { validateEmail } from '../utils/validations';
+
+import Image from 'next/image';
+import { useState } from 'react';
+import ApiService from '../services/api-service';
+import { useRouter } from 'next/router';
+
+interface FormProps {
+  email: string;
+  password: string;
+}
+
+const Login: NextPage = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const formHandler = useForm<FormProps>({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate: {
+      email: (value) =>
+        validateEmail(value) ? null : 'Please, type a valid email',
+      password: (value) => value.length !== 0 ? null : 'Please, type the password'
+    },
+  });
+
+  const onSubmit = async (props: FormProps) => {
+    setLoading(true);
+
+    try {
+      const logged = await ApiService.login({ ...props });
+
+      if (!logged) {
+        return setError('Username or/and password are not correct');
+      }
+
+      return router.replace('/');
+    } catch (error) {
+      setError('Sorry, but something wrong happened. Retry later');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Container my="xl">
+      <Card radius="md">
+        <Container mt="md">
+          <Title align="center">Sign In</Title>
+        </Container>
+
+        <Center>
+          <Box
+            sx={{
+              width: '60%',
+            }}
+            my="md"
+          >
+            <Image
+              src="/assets/logo.png"
+              width="100%"
+              height="25%"
+              layout="responsive"
+              alt="logo"
+            />
+          </Box>
+        </Center>
+        <form onSubmit={formHandler.onSubmit(onSubmit)}>
+          <TextInput
+            id="email-input"
+            label="E-mail"
+            description="E-mail you have used to register"
+            placeholder="your@email.com"
+            mb="md"
+            icon={<MdOutlineAlternateEmail size={16} />}
+            {...formHandler.getInputProps('email')}
+            sx={{
+              'input:focus': {
+                borderColor: 'orange',
+              },
+            }}
+          />
+
+          <PasswordInput
+            id="pwd-input"
+            label="Password"
+            description="Password you have used to register"
+            placeholder="yoursecretpassword"
+            mb="sm"
+            icon={<MdPassword size={16} />}
+            {...formHandler.getInputProps('password')}
+            sx={{
+              'div:focus-within': {
+                borderColor: 'orange',
+              },
+            }}
+          />
+
+          {error && <Alert color="red" mb='sm'>{error}</Alert>}
+
+          <Group position="center">
+            <Button type="submit" color="orange" loading={loading}>
+              Sign In
+            </Button>
+          </Group>
+        </form>
+      </Card>
+    </Container>
+  );
+};
+
+export default Login;
