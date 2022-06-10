d08d486e1823d6882ac9f5727dd100db51ca5c6f
Saliou <gayesaliou29@gmail.com>
2022-06-10 10:53:50 +0200

    chore: create signup page
    Close #6

---
+++ pages/signup.tsx
@@ -0,0 +1,240 @@
+import type { NextPage } from 'next';
+import {
+  Card,
+  Container,
+  Center,
+  TextInput,
+  PasswordInput,
+  Button,
+  Group,
+  Title,
+  Box,
+  Alert,
+  Grid,
+  NumberInput,
+  ActionIcon,
+} from '@mantine/core';
+import { DatePicker } from '@mantine/dates';
+import { MdOutlineAlternateEmail, MdPassword } from 'react-icons/md';
+import { BsSmartwatch, BsCalendarDate } from 'react-icons/bs';
+import { FaWeight } from 'react-icons/fa';
+import { GiBodyHeight } from 'react-icons/gi';
+import { AiOutlineQrcode } from 'react-icons/ai';
+
+import { useForm } from '@mantine/form';
+import { validateEmail } from '../utils/validations';
+
+import Image from 'next/image';
+import { useState } from 'react';
+import ApiService from '../services/api-service';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+
+interface FormProps {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  deviceCode: string;
+  weight: number;
+  height: number;
+  birthday: Date;
+}
+
+const SignUp: NextPage = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const formHandler = useForm<FormProps>({
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      birthday: new Date(),
+      deviceCode: '',
+      height: 177,
+      weight: 66,
+    },
+    validate: {
+      email: (value) =>
+        validateEmail(value) ? null : 'Please, type a valid email',
+      password: (value) =>
+        value.length !== 0 ? null : 'Please, type the password',
+      confirmPassword: (value, values) =>
+        value === values.password ? null : 'Passwords not coincide',
+      height: (value) => (value !== 0 ? null : 'Please, insert a valid height'),
+      weight: (value) => (value !== 0 ? null : 'Please, insert a valid weight'),
+      deviceCode: (value) =>
+        value.length !== 0 ? null : 'Please, insert the device code',
+    },
+  });
+
+  const onSubmit = async (props: FormProps) => {
+    console.log(props);
+    // setLoading(true);
+
+    // try {
+    //   return router.replace('/');
+    // } catch (error) {
+    //   setError('Sorry, but something wrong happened. Retry later');
+    // } finally {
+    //   setLoading(false);
+    // }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>SerenUp - Sign Up</title>
+        <meta name="description" content="Sign Up to the Seren Up Web App" />
+      </Head>
+      <Container my="xl">
+        <Card radius="md">
+          <Container mt="md">
+            <Title align="center">Sign Up</Title>
+          </Container>
+
+          <Center>
+            <Box
+              sx={{
+                width: '60%',
+              }}
+              my="md"
+            >
+              <Image
+                src="/assets/logo.png"
+                width="100%"
+                height="25%"
+                layout="responsive"
+                alt="logo"
+              />
+            </Box>
+          </Center>
+          <form onSubmit={formHandler.onSubmit(onSubmit)}>
+            <Grid>
+              <Grid.Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                <TextInput
+                  id="device-input"
+                  label="Device ID"
+                  description="ID of your smart bracelet"
+                  placeholder="123-32131-31231"
+                  mb="md"
+                  icon={<BsSmartwatch size={16} />}
+                  {...formHandler.getInputProps('deviceCode')}
+                  sx={{
+                    'input:focus': {
+                      borderColor: 'orange',
+                    },
+                  }}
+                  rightSection={
+                      <ActionIcon onClick={() => {
+                          // TODO qr reader
+                          console.log('Open QR Reader');
+                      }}>
+                          <AiOutlineQrcode />
+                      </ActionIcon>
+                  }
+                />
+              </Grid.Col>
+
+              <Grid.Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                <TextInput
+                  id="email-input"
+                  label="E-mail"
+                  description="E-mail you have used to register"
+                  placeholder="your@email.com"
+                  mb="md"
+                  icon={<MdOutlineAlternateEmail size={16} />}
+                  {...formHandler.getInputProps('email')}
+                  sx={{
+                    'input:focus': {
+                      borderColor: 'orange',
+                    },
+                  }}
+                />
+              </Grid.Col>
+              <Grid.Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                <PasswordInput
+                  id="pwd-input"
+                  label="Password"
+                  description="Write the password"
+                  placeholder="yoursecretpassword"
+                  mb="sm"
+                  icon={<MdPassword size={16} />}
+                  {...formHandler.getInputProps('password')}
+                  sx={{
+                    'div:focus-within': {
+                      borderColor: 'orange',
+                    },
+                  }}
+                />
+              </Grid.Col>
+              <Grid.Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                <PasswordInput
+                  id="confirm-pwd-input"
+                  label="Confirm Password"
+                  description="Type again the password"
+                  placeholder="yoursecretpassword"
+                  mb="sm"
+                  icon={<MdPassword size={16} />}
+                  {...formHandler.getInputProps('confirmPassword')}
+                  sx={{
+                    'div:focus-within': {
+                      borderColor: 'orange',
+                    },
+                  }}
+                />
+              </Grid.Col>
+              <Grid.Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                    <DatePicker 
+                        id='birthday-input'
+                        label='Date of birth'
+                        description='Select your birthday'
+                        mb='sm'
+                        icon={<BsCalendarDate size={16} />}
+                        {...formHandler.getInputProps('birthday')}
+                    />
+                </Grid.Col>
+                <Grid.Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                    <NumberInput 
+                        id='weight-input'
+                        label='Weight (kg)'
+                        description="What's your weigth?"
+                        mb='sm'
+                        icon={<FaWeight size={16} />}
+                        {...formHandler.getInputProps('weight')}
+                        hideControls
+                    />
+                </Grid.Col>
+                <Grid.Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                    <NumberInput 
+                        id='height-input'
+                        label='Height (cm)'
+                        description="What's your weigth?"
+                        mb='sm'
+                        icon={<GiBodyHeight size={16} />}
+                        {...formHandler.getInputProps('height')}
+                        hideControls
+                    />
+                </Grid.Col>
+            </Grid>
+
+            {error && (
+              <Alert color="red" mb="sm">
+                {error}
+              </Alert>
+            )}
+
+            <Group position="center">
+              <Button type="submit" color="orange" loading={loading}>
+                Sign Up
+              </Button>
+            </Group>
+          </form>
+        </Card>
+      </Container>
+    </>
+  );
+};
+
+export default SignUp;
