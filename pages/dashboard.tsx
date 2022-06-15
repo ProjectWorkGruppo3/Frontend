fdf931fb29cfca5c7d82fc49bd12e8c45708c82a
Giacomo Baggio <giacomobaggio13@gmail.com>
2022-06-15 14:10:53 +0200

    WIP: Add dummy chart

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,28 +1,65 @@
-import { AppShell, Box, Grid, Group, Navbar } from '@mantine/core';
-import React from 'react';
-import Main from '@components/Main';
-import Sidebar from '@components/Sidebar';
-import Welcome from '@components/Welcome';
-import User from '@components/User';
-import CardsContainer from '@components/CardsContainer';
-import { getRandomInt } from 'lib/utils/getRandomInt';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import Head from 'next/head';
 import Card from '@components/Card';
+import User from '@components/User';
+import Welcome from '@components/Welcome';
+import { AppShell, Box, Grid, Group, Navbar } from '@mantine/core';
+import {
+  CategoryScale,
+  Chart,
+  LinearScale,
+  LineController,
+  LineElement,
+  PointElement,
+  Title,
+} from 'chart.js';
+import { getRandomInt } from 'lib/utils/getRandomInt';
+import Head from 'next/head';
 import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
 import { Logout as LogoutIcon } from 'tabler-icons-react';
+
+Chart.register(
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale
+);
 
 export default function Dashboard() {
   const [steps, setSteps] = useState(0);
   const [heartPulse, setHeartPulse] = useState(0);
   const [dummy, setDummy] = useState(0);
 
+  const [chartData, setChartData] = useState<Number[]>([]);
+
+  const data = {
+    labels: [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ],
+    datasets: [
+      {
+        data: chartData,
+        fill: true,
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)',
+      },
+    ],
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
       setSteps(getRandomInt(10000));
       setHeartPulse(getRandomInt(100));
       setDummy(getRandomInt(10000));
+      setChartData(Array.from({ length: 7 }, () => getRandomInt(10000)));
     }, 5000);
 
     return () => clearInterval(interval);
@@ -36,7 +73,6 @@ export default function Dashboard() {
       <AppShell
         navbar={
           <Navbar
-            height={'100vh'}
             width={{ base: '8%' }}
             style={{
               border: 'none',
@@ -78,6 +114,9 @@ export default function Dashboard() {
             <Card title="Dummy" value={dummy} imageSrc="/assets/steps.svg" />
           </Grid.Col>
         </Grid>
+        <Group position="center" mt={100} style={{ width: '70%' }}>
+          <Line data={data} />
+        </Group>
       </AppShell>
     </Box>
   );
