29ffc6bdbcee820ed8ce1977b39995688537f028
Saliou <gayesaliou29@gmail.com>
2022-06-18 15:21:31 +0200

    fix: fix import

--- context/auth-context.tsx
+++ context/auth-context.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { useContext } from 'react';
-import { User } from '../types/user';
+import { User } from '../models/user';
 
 type AuthProviderProps = {
   children: JSX.Element;
