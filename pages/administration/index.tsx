3d8fa2449ee1551720d3ffb58dfb611be85e7bb9
Saliou <gayesaliou29@gmail.com>
2022-07-04 14:14:23 +0200

    refactor: re-style admin home page

--- pages/administration/index.tsx
+++ pages/administration/index.tsx
@@ -1,24 +1,12 @@
-import { Box, Center, Grid } from '@mantine/core';
+import { Box, Center, Grid, Title } from '@mantine/core';
+import { EaseInOutDiv, FadeInDiv, Floating } from 'animations';
 import { NextPage } from 'next';
-import {
-  AnalysisStatCard,
-  StatCard,
-  TitleLink
-} from '../../components/administration/index';
 
-import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { CardFadeIn, FadeInDiv } from '../../animations';
 import { CircularLoading, Header } from '../../components/common';
 import { useAuth } from '../../context/auth-context';
-import { normalDate } from '../../utils/date-format';
-
-const DensityMap = dynamic(
-  () => import('../../components/administration/density-map'),
-  { ssr: false }
-);
 
 const AdministrationPage: NextPage = () => {
   const auth = useAuth();
@@ -38,13 +26,118 @@ const AdministrationPage: NextPage = () => {
   }
 
   return (
-    <>
+    <Box sx={{ width: '100%', height: '100vh' }} p={0}>
       <Head>
         <title>Administration Seren Up</title>
         <meta name="description" content="Seren Up Web App" />
         <link rel="icon" href="/assets/logo.png" />
       </Head>
-      <Box py="xl" px="1%">
+      <Grid sx={{ width: '100%', height: '100%' }} m={0}>
+        <Grid.Col span={6} p='xl'>
+          <FadeInDiv>
+            <Header
+              title="Admministration Panel"
+              onLogout={() => {
+                setLoading(true);
+                auth!.setAuthState(null);
+              }}
+            />
+          </FadeInDiv>
+          {/* {loading ? (
+            <Center>
+              <FadeInDiv>
+                <CircularLoading />
+              </FadeInDiv>
+            </Center>
+          ) : (
+            <>
+              <Grid justify="flex-start" align="center" mb="sm">
+                <Grid.Col xs={12} sm={12} md={12} lg={2} xl={2}>
+                  <CardFadeIn>
+                    <StatCard
+                      name="Admins"
+                      value={20}
+                      onClick={() => router.push('/administration/admins')}
+                    />
+                  </CardFadeIn>
+                </Grid.Col>
+                <Grid.Col xs={12} sm={12} md={12} lg={2} xl={2}>
+                  <CardFadeIn>
+                    <StatCard name="Bracelets" value={200} />
+                  </CardFadeIn>
+                </Grid.Col>
+              </Grid>
+
+              <Box mb="lg">
+                <FadeInDiv>
+                  <Box mb="sm">
+                    <TitleLink
+                      link="/administration/analysis"
+                      title="Analysis"
+                    />
+                  </Box>
+
+                  <Grid>
+                    <Grid.Col span={2}>
+                      <AnalysisStatCard title="Analysis 1" value={80} />
+                    </Grid.Col>
+                    <Grid.Col span={2}>
+                      <AnalysisStatCard title="Analysis 1" value={80} />
+                    </Grid.Col>
+                  </Grid>
+                </FadeInDiv>
+              </Box>
+
+              <Box mb="lg">
+                <FadeInDiv>
+                  <Box mb="xs">
+                    <TitleLink
+                      link="/administration/reports"
+                      title="Last Reports"
+                    />
+                  </Box>
+                </FadeInDiv>
+                <Grid>
+                  {Array.from({ length: 4 }, (v, k) => (
+                    <Grid.Col span={2}>
+                      <CardFadeIn>
+                        <StatCard
+                          name={normalDate(new Date())}
+                          value={`Report #${k}`}
+                        />
+                      </CardFadeIn>
+                    </Grid.Col>
+                  ))}
+                </Grid>
+              </Box>
+            </>
+          )} */}
+        </Grid.Col>
+        <Grid.Col span={6} p={0}>
+          <Box
+            sx={{
+              backgroundColor: 'var(--fi-color)',
+              height: '100%',
+            }}
+            p="lg"
+            mb="xl"
+          >
+            <EaseInOutDiv>
+              <Title align="right" order={3}>
+                Seren-Up Administration
+              </Title>
+              <Center>
+                <Floating>
+                  <Box py="xl">
+                    <img src="/assets/admin.png" />
+                  </Box>
+                </Floating>
+              </Center>
+            </EaseInOutDiv>
+          </Box>
+        </Grid.Col>
+      </Grid>
+      {/* <Box py="xl" px="1%">
         <FadeInDiv>
           <Header
             title="Admministration Panel"
@@ -120,8 +213,8 @@ const AdministrationPage: NextPage = () => {
             </Box>
           </>
         )}
-      </Box>
-    </>
+      </Box> */}
+    </Box>
   );
 };
 
